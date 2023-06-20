85d06e7036ef74455b72b48f848e794f94773639
azuressu <martina0130@naver.com>
2023-06-20 14:48:45 +0900

    init commit

---
+++ src/main/java/com/sparta/springresttemplateserver/controller/ItemController.java
@@ -0,0 +1,41 @@
+package com.sparta.springresttemplateserver.controller;
+
+
+import com.sparta.springresttemplateserver.dto.ItemResponseDto;
+import com.sparta.springresttemplateserver.dto.UserRequestDto;
+import com.sparta.springresttemplateserver.entity.Item;
+import com.sparta.springresttemplateserver.service.ItemService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/server")
+public class ItemController {
+
+    // 클라이언트 입장에서 RestTemplate로 보내온 요청을 받는 Controller임
+
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping("/get-call-obj")
+    public Item getCallObject(@RequestParam String query) {
+        return itemService.getCallObject(query);
+    }
+
+    @GetMapping("/get-call-list")
+    public ItemResponseDto getCallList() {
+        return itemService.getCallList();
+    }
+
+    @GetMapping("/post-call/{query}")
+    public Item postCall(@PathVariable String query, @RequestBody UserRequestDto requestDto) {
+        return itemService.postCall(query, requestDto);
+    }
+
+    @GetMapping("/exchange-call")
+    public ItemResponseDto exchangeCall(@RequestHeader("X-Authorization") String token, @RequestBody UserRequestDto requestDto) {
+        return itemService.exchangeCall(token, requestDto);
+    }
+}
